4fb5b07beb62d81fdb5f721530dea59b425bed32
Alvaro Ruano <alvaroruano@gmail.com>
2026-07-28 19:48:27 +0200

    Release v1.0.6: build.gradle update with new app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.equivalencias.crisfiit"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 8
+        versionName = "1.0.6"
     }
 
     compileOptions {
